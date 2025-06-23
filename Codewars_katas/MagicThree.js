2e653a386c33fe99fc6eae9635f39d35b9bcb306
The Gym Rwanda <The Gym Rwanda@TUMBALAB3>
2025-06-23 11:23:52 +0200

    sortProducts

--- Codewars_katas/MagicThree.js
+++ Codewars_katas/MagicThree.js
@@ -5,7 +5,7 @@ const MagicThree = (Arr)=>{
     for( let i=0 ; i<n ; i ++){
         for( let j=0; j<i ; j++){
             for( let k=0; k<j; k++){
-                if( Arr[i]+Arr[j]+Arr[j]===0){
+                if( Arr[i]+Arr[j]+Arr[k]===0){
                     console.log("true")
                     return true
                 }
@@ -19,4 +19,4 @@ const MagicThree = (Arr)=>{
     }
 }
 
-console.log(MagicThree([1,2,3,4,5,5,6,65,4,3,5,3,3]))
+console.log(MagicThree([0]))
---
+++ js-practical-exercises/Sort-products.js
@@ -0,0 +1,5 @@
+
+
+const products =[{product:"mango",Price:400},{product:"casava",Price:100},{product:"rice",Price:600},]
+products.sort((a,b)=> a.Price - b.Price)
+console.log(products)
