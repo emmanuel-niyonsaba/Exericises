8bef4acc54b29580218ece7ca4a402f2ee2b2f60
The Gym Rwanda <The Gym Rwanda@TUMBALAB3>
2025-06-24 08:25:15 +0200

    refactor : add condition in isThreeMagic

--- Codewars_katas/MagicThree.js
+++ Codewars_katas/MagicThree.js
@@ -1,7 +1,14 @@
 
 
 const MagicThree = (Arr)=>{
+    const zeros = Arr.filter((a)=>a===0).length
+    if(zeros >= 3){
+        return true
+    }
     const n= Arr.length
+    if( n <3){
+        return false
+    }
     for( let i=0 ; i<n ; i ++){
         for( let j=0; j<i ; j++){
             for( let k=0; k<j; k++){
