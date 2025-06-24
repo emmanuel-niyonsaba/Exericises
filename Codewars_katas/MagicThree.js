754c1f6bdb69a903fc4f5806e0a07227b8a71518
The Gym Rwanda <The Gym Rwanda@TUMBALAB3>
2025-06-24 08:29:48 +0200

    refactor : add easy way

--- Codewars_katas/MagicThree.js
+++ Codewars_katas/MagicThree.js
@@ -27,3 +27,12 @@ const MagicThree = (Arr)=>{
 }
 
 console.log(MagicThree([0]))
+
+///// here there is easy way to do it by for..of
+function isMagicThree(arr) {
+  for(x of arr)
+  for(y of arr)
+  for(z of arr)
+  if(x + y + z === 0) return true;
+  return false;
+}
