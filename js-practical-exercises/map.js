17f89d5ab76c92532746bbfcbcd183115b692acb
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-02 19:23:15 +0200

    add for...of loop

--- js-practical-exercises/map.js
+++ js-practical-exercises/map.js
@@ -4,4 +4,7 @@ let myMap = new Map()
 myMap.set("name","Emmanuel")
 myMap.set("school","Tumab-college")
 myMap.set("class","IT-A")
-console.log(myMap)
+
+for(let elem of myMap){
+    console.log(`data is ${elem}`)
+}
