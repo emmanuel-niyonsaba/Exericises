7e468969603e86088b4159bf751daa4c875203ec
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-04 10:26:43 +0200

    done with replace alphebet with postion

--- Codewars_katas/replaceAlphabet.js
+++ Codewars_katas/replaceAlphabet.js
@@ -4,16 +4,13 @@
 function alphabetPosition(text) {
   const alps =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
                           
-                      
+        let result =[] ;      
   const lower = text.toLowerCase()
   for( let letter of lower){
-   for( let   alp of alps ){
-     if (letter === alp){
-       console.log(indexOf(alp))
-       
-     }
-   }
-  }
-  return "done"
+    if(alps.includes(letter)){
+        result.push(alps.indexOf(letter)+1)
+    }
 }
-console.log(alphabetPosition("emmyson"))
+return result.join(" ")
+}
+console.log(alphabetPosition("The sunset sets at twelve o' clock."))
