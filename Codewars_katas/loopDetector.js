ff96f4de1b091ab1673fac30a21f2a217a35636a
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-07 09:00:44 +0200

    done with loopDetector exerciese

--- Codewars_katas/loopDetector.js
+++ Codewars_katas/loopDetector.js
@@ -1,5 +1,12 @@
 
 const hasLoop =(arr)=>{
     let visted = new Set()
-    if( )
+    for(i =0 ; i<arr.length ; i= arr[i]){
+        if(visted.has(i)){
+            return true
+        }
+        visted.add(arr[i])
+    }
+    return false
 }
+console.log(hasLoop([1,2,3,5,7,9]))
