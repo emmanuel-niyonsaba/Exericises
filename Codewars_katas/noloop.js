6bf014e6ee5b9bcbef0420d388c01630784e2921
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-11 08:36:54 +0200

    done by smallEnough exercises

--- Codewars_katas/noloop.js
+++ Codewars_katas/noloop.js
@@ -2,5 +2,8 @@
 // This funciton is to find if the give array contain a element which may be greater than limit
 
 const smallEnough =(arr,limit)=>{
-    
+    // this .every method help to iterate trhoug any arr and check the condition to each element and return true or false
+    return arr.every(el=>el<=limit)
+
 }
+console.log(smallEnough([1,2,3,4,4,5,6,7],6))
