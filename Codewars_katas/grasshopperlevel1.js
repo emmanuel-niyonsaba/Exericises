1c74c71e89738395a74459260969faea9474766b
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-16 21:47:49 +0200

    add grasshopper level1

---
+++ Codewars_katas/grasshopperlevel1.js
@@ -0,0 +1,10 @@
+class Hero {
+  // add default values here
+    constructor(name="Hero") {
+      this.name= name
+      this.position= "00"
+      this.health= 100
+      this.damage = 5
+      this.experience= 0
+    }
+}
