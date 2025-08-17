b65542de6a78988435d12de0d4f7dc171921814d
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-17 19:43:51 +0200

    done with plus-mins-plus

--- Codewars_katas/plus-nimus.js
+++ Codewars_katas/plus-nimus.js
@@ -1,16 +1,10 @@
-
 function catchSignChange(arr) {
-  if (arr.length < 1) return 0;
-
+  if (!arr || arr.length === 0) return 0;
   let count = 0;
-
-  for (let i = 0; i < arr.length ; i++) {
-    if ((arr[i] > 0 && arr[i + 1] <= 0) ||
-        (arr[i] < 0 && arr[i + 1] >= 0) ||
-        (arr[i] === 0 && arr[i + 1] !== 0)) {
+  for (let i = 1; i < arr.length; i++) {
+    if ((arr[i - 1] >= 0 && arr[i] < 0) || (arr[i - 1] < 0 && arr[i] >= 0)) {
       count++;
     }
   }
-
   return count;
 }
