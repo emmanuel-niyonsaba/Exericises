773fc791d7b7be472340ef9cd6ae7a0e0b64606d
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-19 10:56:35 +0200

    add simple regEx

--- Codewars_katas/passwordRegexValidation.js
+++ Codewars_katas/passwordRegexValidation.js
@@ -1,4 +1,15 @@
 
 
 // assign your RegExp to REGEXP:
-const REGEXP = /./;
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a digit
+// only contains alphanumeric characters (note that '_' is not alphanumeric)
+
+
+const passwordValidation  =(password) =>{
+    return /^([\w]{6}..)$/i.test(password)
+}
