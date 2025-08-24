cce24c24c9905fa28f34a592a806b1cb5e076a80
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-24 19:50:37 +0200

    fix: handle the empty array

--- Codewars_katas/avgCal.js
+++ Codewars_katas/avgCal.js
@@ -1,5 +1,7 @@
 
 function findAverage(array) {
+  if(array.length <1) return 0
   return array.reduce((a,b)=>a+b)/array.length
 }
 console.log(findAverage([1,2,3,4,5,6,7,8]))
+console.log(findAverage([]))
