c4944f1bcc7d858e06fff9be5c848351d57d7fe9
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-30 23:08:55 +0200

    finalize the filterlist exercises

--- Codewars_katas/filterlist.js
+++ Codewars_katas/filterlist.js
@@ -1,11 +1,2 @@
-
-function filter_list(l) {
-    let result=[]
-    l.map(e=>{
-        if(!typeof e ==="String"){
-            result.push(e)
-        }
-    })
-    return result
-}
+g
 console.log(filter_list([1,2,'aasf','1','123',123]))
