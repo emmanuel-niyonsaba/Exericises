1e269ebad964c8139a56e0914b79d007c66b09c6
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-09-02 08:53:00 +0200

    add: lowest and highest exercises

--- Codewars_katas/filterlist.js
+++ Codewars_katas/filterlist.js
@@ -1,2 +1,11 @@
-g
+
+function filter_list(l) {
+    let result=[]
+    l.map(e=>{
+        if(typeof e ==="number"){
+            result.push(e)
+        }
+    })
+    return result
+}
 console.log(filter_list([1,2,'aasf','1','123',123]))
