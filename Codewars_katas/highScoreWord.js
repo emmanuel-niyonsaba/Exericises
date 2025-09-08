d3496592cfae641ae53bf029d271055acc09e36d
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-09-08 23:51:25 +0200

    add find the highScoreWord

---
+++ Codewars_katas/highScoreWord.js
@@ -0,0 +1,7 @@
+
+function high(x){
+const arr =x.split(" ")
+let arph= [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+for()
+}
+console.log(high('man i need a taxi up to ubud'))
