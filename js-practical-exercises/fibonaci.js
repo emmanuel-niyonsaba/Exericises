c7d6cab48f139c30d7360bf9b9604c918bf269b5
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-10-01 10:52:52 +0200

    add easy logic of fibonci number

---
+++ js-practical-exercises/fibonaci.js
@@ -0,0 +1,16 @@
+
+
+function fibonaci(frenq){
+    let a =0
+    let b =1
+    let c=0
+    console.log(c)
+    for(i=1;i<frenq;i++){
+
+        a=b
+        b=c
+        c=a+b;
+        console.log(c)
+    }
+    }
+    fibonaci(5);
