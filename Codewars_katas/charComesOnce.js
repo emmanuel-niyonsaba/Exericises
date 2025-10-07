7ed7adb70dc7e955d58554e0ff01485571e28c8d
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-10-08 01:09:47 +0200

    addition exercises

--- Codewars_katas/charComesOnce.js
+++ Codewars_katas/charComesOnce.js
@@ -1,17 +1,20 @@
 
-function firstNonRepeatingLetter(s) {
-  const arr = s.split('');
-  const lower = s.toLowerCase();
+// function firstNonRepeatingLetter(s) {
+//   const arr = s.split('');
+//   const lower = s.toLowerCase();
 
-  for (let i = 0; i < arr.length; i++) {
-    if (lower.indexOf(lower[i]) === lower.lastIndexOf(lower[i])) {
-      return arr[i];
-    }
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (lower.indexOf(lower[i]) === lower.lastIndexOf(lower[i])) {
+//       return arr[i];
+//     }
+//   }
 
-  return ''; 
+//   return null; 
+
+// }
+// console.log(firstNonRepeatingLetter("sTreSS"))
 
 
-}
+let arr =[1,2,4,3,3,1,3,[2,3,4,7,223]]
+console.log([...new Set(arr.flat(Infinity))])
 
-console.log(firstNonRepeatingLetter("sTreSS"))
