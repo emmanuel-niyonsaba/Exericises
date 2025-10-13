43d57e54f55ae7237aec7233495eb12ff07c7b1f
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-10-13 08:04:29 +0200

    add xandohs

---
+++ Codewars_katas/xandoh's.js
@@ -0,0 +1,28 @@
+
+
+Obsolete
+7 kyu
+Exes and Ohs
+JavaScript:
+function XO(str) {
+//     let x= str.match(/x/i).length;
+//   console.log(x)
+//   console.log("Here!")
+//   let o= str.match(/o/i).length;
+// //   console.log(o)
+  
+// return  x==o?true:false
+  let lower = str.toLowerCase()
+  
+  let xarr=0;
+  let oarr =0;
+  for(let x of lower){
+    if(x=="x"){
+    xarr +=1
+    }
+    if(x=="o"){
+      oarr +=1
+    }
+  }
+  return xarr==oarr?true:false
+}
