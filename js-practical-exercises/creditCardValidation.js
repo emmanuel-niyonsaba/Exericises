f1804c799efa22c4f9b0df50c9ea35f544248cdd
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-10-22 23:07:28 +0200

    respective regEx comment

--- js-practical-exercises/creditCardValidation.js
+++ js-practical-exercises/creditCardValidation.js
@@ -1,6 +1,7 @@
 
 //this code will check and validate a credit card number format
 const ValidateCard=(cardNumber)=>{
+  // this regex will check for the format of 4 sets of 4 digits separated by hyphens
     const regEx= /^(\d{4}-){3}\d{4}$/;
     // let a = /^(\d{4}-){3}\d{4}/;
     const checCard = regEx.test(cardNumber);
