2b3e8fb9f72f3f6b2771a7f3ea013d290079796f
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-10-25 21:45:20 +0200

    add respective commnt

--- Codewars_katas/simpleCal.js
+++ Codewars_katas/simpleCal.js
@@ -1,10 +1,12 @@
 
+// A simple calculator function that performs basic arithmetic operations
 function calculator(a, b, sign) {
-
+// Validate inputs are numbers 
   if (typeof a !== 'number' || typeof b !== 'number') {
+    // If inputs are not numbers, return 'unknown value'
     return 'unknown value';
   }
-
+// 
   switch (sign) {
     case '+':
       return a + b;
@@ -14,6 +16,7 @@ function calculator(a, b, sign) {
       return a * b;
     case '/':
       return a / b;
+      // If the operation sign is not recognized, return 'unknown value'
     default:
       return 'unknown value';
   }
